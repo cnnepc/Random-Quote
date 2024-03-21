a48eeb7faba0a773226e76dc95488fba193d9df5
cnnepc <162931146+cnnepc@users.noreply.github.com>
2024-03-20 23:46:38 -0400

    Added copy quote button functionality

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const quote = document.querySelector(".quote");
   const tagsContainer = document.querySelector(".tags-container");
   const newQuote = document.querySelector(".new-quote");
+  const copyQuote = document.querySelector(".copy-quote");
 
   async function updateContent() {
     const response = await fetch("https://api.quotable.io/quotes/random");
@@ -24,8 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  newQuote.addEventListener("click", updateContent);
+  async function copyToClipboard() {
+    try {
+      await navigator.clipboard.writeText(quote.textContent);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
   
+  newQuote.addEventListener("click", updateContent);
+  copyQuote.addEventListener("click", copyToClipboard);
 
   updateContent();
 });
